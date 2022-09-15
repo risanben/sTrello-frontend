09ce5bc56ebcb9d1752195c6ab49816e904f5784
Maor Layani <maorlayani@gmail.com>
2022-09-15 19:14:32 +0300

    Add cmp add new task

--- src/cmps/group-list.jsx
+++ src/cmps/group-list.jsx
@@ -2,23 +2,41 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom'
 import { GroupEdit } from './group-edit';
 import { GroupPreview } from './group-preview'
+import { useDispatch } from "react-redux";
+import { updateBoard } from '../store/board.actions'
+import { useEffect } from 'react';
 
 export const GroupList = ({ board }) => {
 
     const [isAddingGroup, setIsAddingGroup] = useState(false)
-
+    const [currBoard, setCurrBoard] = useState(board)
+    useEffect(() => {
+        console.log(currBoard)
+    }, [currBoard])
+    const dispatch = useDispatch()
     const onAddingGroup = () => {
         setIsAddingGroup(!isAddingGroup)
     }
 
-    console.log('board.groups', board.groups);
+    const addTask = async (groupToUpdate) => {
+        const boardToSave = { ...board }
+        boardToSave.groups.map(group => (group.id === groupToUpdate.id) ? groupToUpdate : group)
+        const savedBoard = await dispatch(updateBoard(boardToSave))
+        console.log('savedBoard', savedBoard)
+        setCurrBoard(savedBoard.board)
+    }
+
+
+
+    // console.log('board.groups', currBoard.groups);
+    if (!currBoard) return <div>Loading...</div>
     return (
         <section className="group-list">
-            {board.groups.map(group => {
-                return <GroupPreview key={group.id} group={group} />
+            {currBoard.groups.map(group => {
+                return <GroupPreview key={group.id} group={group} addTask={addTask} />
             })}
             {!isAddingGroup && <button onClick={onAddingGroup}>Add group</button>}
-            {isAddingGroup && <GroupEdit onAddingGroup={onAddingGroup} board={board} />}
+            {isAddingGroup && <GroupEdit onAddingGroup={onAddingGroup} board={currBoard} />}
 
             {/* <Link to="/group/edit" className='nice-button'>Add group</Link> */}
         </section>
