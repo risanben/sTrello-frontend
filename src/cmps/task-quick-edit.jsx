965bb8a151dfd2bedea4e006f82b564d6aedab24
risanBenichou <ris.benichou@gmail.com>
2022-09-21 08:56:30 +0300

    fix quick task bug,

--- src/cmps/task-quick-edit.jsx
+++ src/cmps/task-quick-edit.jsx
@@ -7,11 +7,10 @@ import { TaskLabel } from './task-label.jsx'
 import { useState, useRef, useEffect } from 'react'
 import { LabelModal } from './label-modal'
 import { TaskMember } from './task-members.jsx'
+import { Link } from 'react-router-dom'
 
 
 export const TaskQuickEdit = ({ task, pos }) => {
-
-  // console.log('pos.style:', pos.style)
 
   const [title, setTaskTitle] = useState(task.title)
   const [labelModal, setLabelModal] = useState(false)
@@ -24,7 +23,6 @@ export const TaskQuickEdit = ({ task, pos }) => {
   const handleClickOutside = (e) => {
     if (!refLabelModal.current) return
     if (!refLabelModal.current.contains(e.target)) {
-      //todo: close modal
       setLabelModal(false)
     }
   }
@@ -39,7 +37,6 @@ export const TaskQuickEdit = ({ task, pos }) => {
       ...task
     }
     task.title = title
-    // console.log('task:', task)
   }
 
   const openLabelModal = () => {
@@ -50,22 +47,11 @@ export const TaskQuickEdit = ({ task, pos }) => {
     ev.stopPropagation()
   }
 
-  const _getTaskCoverStyle = (bg) => {
-    // console.log('bg:', bg)
-    if (bg.color) {
-      return bg.color
-    }
 
-    return "red"
-
-  }
-
-// console.log('style:', style)
-  return <section className="task-quick-edit" onClick={onEditClick} >
-    {/* <div className="dark-screen"></div> */}
-    <div className='left-col' style={{top:`${pos.top}`, left:`${pos.left}`}}>
-      {task.style?.bg?.imgUrl && <div className='task-cover' style={{ backgroundImage: `url(${task.style.bg.imgUrl})`, height: "180px", backgroundSize: "cover" }}></div>}
-      {task.style?.bg?.color && <div className='task-cover' style={{ background: _getTaskCoverStyle(task.style.bg), height: "32px"}}></div>}
+  return <section className="task-quick-edit" onClick={onEditClick} style={{...pos.style}}>
+    <div className='left-col' style={{width: pos.position.width}}>
+      {task.style?.bg?.imgUrl && <div className='task-cover' style={{backgroundImage: `url(${task.style.bg.imgUrl})`, height:"180px", backgroundSize:"cover",width:`${pos.width}px`}}></div>}
+      {task.style?.bg?.color && <div className='task-cover' style={{background:`${task.style.bg.color}`, height:"32px",width:`${pos.width}px`}}></div>}
       <div className='input-side'>
         <section className='labels'>
           {task?.labelIds && <TaskLabel labelIds={task.labelIds} />}
@@ -78,13 +64,14 @@ export const TaskQuickEdit = ({ task, pos }) => {
       <button className='btn-add'>Save</button>
     </div>
     <ul className="quick-edit-actions">
+      <Link to={`task/details`}> 
       <li><FaWindowMaximize /> Open card</li>
+      </Link>
       <li onClick={openLabelModal}><BsTagFill /> Edit labels</li>
       {labelModal && <section ref={refLabelModal}><LabelModal /></section>}
       <li><HiUser /> Change members</li>
       <li><HiArchive /> Archive</li>
     </ul>
-
   </section>
 
 }
