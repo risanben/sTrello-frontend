d92d0e6e4deaa7902d377e59bbb888fff5a25689
risanBenichou <ris.benichou@gmail.com>
2022-09-21 13:58:35 +0300

    start label sass

--- src/cmps/task-details-labels-modal.jsx
+++ src/cmps/task-details-labels-modal.jsx
@@ -2,10 +2,10 @@ import { TaskDetailsLabel } from "./task-details-label"
 
 export const TaskDetailsLabelModal = ({ labelIds, onSetLabel ,toggleLabelsModal }) => {
     return (
-        <section className="members-modal">
+        <section className="labels-modal">
             {/* <div className="members-modal-container"> */}
             <div onClick={toggleLabelsModal}>X</div>
-                <div className="members-modal-title">Labels</div>
+                <div className="labels-modal-title">Labels</div>
                 <input type="text" placeholder="Search Labels..." />
                 <h3>Labels</h3>
                 <section className="member-detail-container">
